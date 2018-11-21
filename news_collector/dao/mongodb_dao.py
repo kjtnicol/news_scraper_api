28111f99f347a528f67894cbbf27b83d3155d93d
Nicholas Kim <kjtnicol@gmail.com>
2018-11-21 13:39:33 +1100

    added tag_prediction jupyter file for machine  learning

--- news_collector/dao/mongodb_dao.py
+++ news_collector/dao/mongodb_dao.py
@@ -2,12 +2,25 @@ from pymongo import MongoClient, TEXT, errors
 import json
 import re
 from news_collector import app_config
+import pandas as pd
 
 
 class MongoDbDao:
     def __init__(self, conn_string, db_name=app_config.db_name):
         self.client = MongoClient(conn_string)
         self.db = self.client[db_name]
+
+    def get_df_from_db(self, collection_name, query={}, no_id=True):
+        # Make a query to the specific DB and Collection
+        cursor = self.db[collection_name].find(query)
+
+        # Expand the cursor and construct the DataFrame
+        df = pd.DataFrame(list(cursor))
+
+        if no_id:
+            del df['id']
+
+        return df
 
     def upload_df(self, df, collection_name):
         # Use try and except to avoid inserting duplicate entries (url is an unique index).
